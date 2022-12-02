e88e5ffde5db06eee567a313b12ef8f80ffeaad8
Quang Huy <phamquanghuy2809@gmail.com>
2022-12-02 21:30:11 +0700

    video4-bootstrap5

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,10 @@
 const getHomePage = (req, res) => {
     return res.render("home.ejs");
 };
+const getUserPage = (req, res) => {
+    return res.render("user.ejs");
+};
 export default {
     getHomePage,
+    getUserPage,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -4,6 +4,7 @@ import homeController from "../controllers/homeController";
 const router = express.Router();
 const initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
+    router.get("/user", homeController.getUserPage);
     app.use("/", router);
 };
 
