2fec72baee8b007875961bdfb4f374065f858118
Quang Huy <phamquanghuy2809@gmail.com>
2022-12-03 16:59:53 +0700

    rename table user

--- src/seeders/seeders/20221203094038-demo-user.js
+++ src/seeders/seeders/20221203094038-demo-user.js
@@ -12,7 +12,7 @@ module.exports = {
          * }], {});
          */
         await queryInterface.bulkInsert(
-            "Users",
+            "User",
             [
                 {
                     email: "example1@gm.com",
